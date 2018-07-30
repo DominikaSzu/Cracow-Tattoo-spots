30f95c6ecd939d8a6c0a9fde484a2b25978d78a7
Dominika Szukała <dominika.szukala@gmail.com>
2018-07-30 20:00:03 +0200

    Feat: Markers displayed on page

--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 class Map extends Component {
+     state = {
+        locations: [
+            {name: 'Rock’n’Ink', location: { lat: 50.065207, lng: 19.938842}, id: 1},
+            {name: 'Crazy Tattoo', location: { lat: 50.059621, lng: 19.937855}, id: 2},
+            {name: 'Retro Ink Tattoo', location: { lat: 50.062594, lng: 19.934261}, id: 3},
+            {name: 'Kult Tattoo Fest', location: { lat: 50.062911, lng: 19.941253}, id: 4},
+            {name: 'Hardcore Tattoo', location: { lat: 50.063879, lng: 19.937382}, id: 5}
+        ]
+    }
+    
+    
     render() {
-        
+        let locations = this.state.locations;
         const AppGoogleMap = withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = {{ lat: 50.049683, lng: 19.944544 }}
-        defaultZoom = { 8 }
+        defaultCenter = {{ lat: 50.061599, lng: 19.937331 }}
+        defaultZoom = { 15 }
       >
+        {locations.map(location => 
+        <Marker 
+            position={location.location}
+            key={location.id}
+            />
+            )}
       </GoogleMap>
    ));
         
