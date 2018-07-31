3b39dd2e2ddfc23ae876293560ff605f6cbb8444
Dominika Szukała <dominika.szukala@gmail.com>
2018-07-31 21:29:05 +0200

    Feat: state to MapContainer added

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -5,6 +5,16 @@ import { GoogleApiWrapper } from 'google-maps-react'
 
 class MapContainer extends Component {
     
+    state={
+            locations: [
+            {name: 'Rock’n’Ink', location: { lat: 50.065207, lng: 19.938842}, id: 1},
+            {name: 'Crazy Tattoo', location: { lat: 50.059621, lng: 19.937855}, id: 2},
+            {name: 'Retro Ink Tattoo', location: { lat: 50.062594, lng: 19.934261}, id: 3},
+            {name: 'Kult Tattoo Fest', location: { lat: 50.062911, lng: 19.941253}, id: 4},
+            {name: 'Hardcore Tattoo', location: { lat: 50.063879, lng: 19.937382}, id: 5}
+        ]
+    }
+    
     componentDidUpdate(prevProps, prevState) {
             if (prevProps.google !== this.props.google) {
                 this.loadMap();
